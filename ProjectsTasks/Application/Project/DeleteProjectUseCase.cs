246838afb940c446f23a31eac4fcb53635d970fa
PAULO MATIAS <pgabrieldeveloper@outlook.com>
2024-02-12 09:39:04 -0300

    feat(project): use case para excluir um projeto

---
+++ ProjectsTasks/Application/Project/DeleteProjectUseCase.cs
@@ -0,0 +1,20 @@
+﻿using ProjectsTasks.Infrastruct.Database.entities;
+using ProjectsTasks.Infrastruct.Database.Repository.Interfaces;
+
+namespace ProjectsTasks.Application.Project
+{
+    public class DeleteProjectUseCase : UnitUseCase<int>
+    {
+        private readonly IProjectRepository projectRepository;
+
+        public DeleteProjectUseCase(IProjectRepository projectRepository)
+        {
+            this.projectRepository = projectRepository;
+        }
+
+        public void Execute(int input)
+        {
+            projectRepository.Delete(input, new Infrastruct.Database.entities.Project { Id = input });
+        }
+    }
+}
