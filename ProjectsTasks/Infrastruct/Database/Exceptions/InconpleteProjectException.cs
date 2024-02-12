e1a51d5d78191779f41b4fe06782a9ee8bc5cac1
PAULO MATIAS <pgabrieldeveloper@outlook.com>
2024-02-12 09:38:07 -0300

    adicionando Exceções personalizadas

---
+++ ProjectsTasks/Infrastruct/Database/Exceptions/InconpleteProjectException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectsTasks.Infrastruct.Database.Exceptions
+{
+    public class IncompleteProjectException : Exception
+    {
+        public IncompleteProjectException(string message) : base($"Projeto so pode ser excluido quando todas tarefaz estiverem concluidas\n Tarefas a serem concluidas: {message}") { }
+    }
+}
---
+++ ProjectsTasks/Infrastruct/Database/Exceptions/LimitTaskException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectsTasks.Infrastruct.Database.Exceptions
+{
+    public class LimitTaskException : Exception
+    {
+        public LimitTaskException() : base("Limite máximo de tarefas excedido. O limite é 20 tarefas") { }
+    }
+}
